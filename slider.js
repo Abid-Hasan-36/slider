b0afdfa7c9e0a77af0700759a3aea1b7fc7f0b20
Abid Hasan <abidhasand36@gmail.com>
2023-09-08 01:42:46 +0600

    Added Auto sliding effect

--- slider.js
+++ slider.js
@@ -1,25 +1,45 @@
 let sld = document.querySelectorAll('.slider');
-var i=0;
-function next(){
+// condition for next slide
+var i = 0;
+function next() {
     sld[i].style.animation = ' nxt1 0.5s ease-in forwards';
-    if(i>= sld.length -1){
-        i=0;
+    if (i >= sld.length - 1) {
+        i = 0;
     }
-    else{
+    else {
         i++;
-        
+
     }
-sld[i].style.animation=  'nxt2 0.5s ease-in forwards';
+    sld[i].style.animation = 'nxt2 0.5s ease-in forwards';
+}
+// condition for previous slide
+
+function prev() {
+    sld[i].style.animation = ' prev1 0.5s ease-in forwards';
+    if (i == 0) {
+        i = sld.length - 1;
+    }
+    else {
+        i--;
+
+    }
+    sld[i].style.animation = 'prev2 0.5s ease-in forwards';
 }
 
-function prev(){
-    sld[i].style.animation = ' prev1 0.5s ease-in forwards';
-    if(i==0){
-        i=sld.length-1;
+// autometic slide show!
+
+function start(){
+    stop = setInterval(timer,2000);
+    function timer(){
+        next();
     }
-    else{
-        i--;
-        
-    }
-sld[i].style.animation=  'prev2 0.5s ease-in forwards';
+
 }
+start();
+// stop when mouseover
+const stp =document.querySelector('.container');
+ stp.addEventListener('mouseover',function(){
+    clearInterval(stop);
+ })
+// restart autosliding
+stp.addEventListener('mouseout',start);
